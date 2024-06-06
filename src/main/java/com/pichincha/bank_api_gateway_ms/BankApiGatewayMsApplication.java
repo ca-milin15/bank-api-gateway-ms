55512203a8cb66f3266f0fb30c1dd6844570f8c5
User Developer <backenddeveloper30@gmail.com>
2024-06-05 22:29:03 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/pichincha/bank_api_gateway_ms/BankApiGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_api_gateway_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiGatewayMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/bank_api_gateway_ms/BankApiGatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.bank_api_gateway_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiGatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
